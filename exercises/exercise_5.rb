0fc156ed6f7bac2bd649ba70065e1f26ff5e8c53
Jamaal <jamaal729@yahoo.com>
2019-12-10 05:11:00 +0000

    Complete exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -1,10 +1,20 @@
+# frozen_string_literal: true
+
 require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
 require_relative './exercise_3'
 require_relative './exercise_4'
 
-puts "Exercise 5"
-puts "----------"
+puts 'Exercise 5'
+puts '----------'
 
 # Your code goes here ...
+@total_revenue = Store.sum('annual_revenue')
+puts "All stores: total revenue: #{@total_revenue}"
+
+@average_revenue = Store.average('annual_revenue')
+puts "All stores: average revenue: #{@average_revenue}"
+
+@stores_with_over_million_sales = Store.where('annual_revenue >= 1000000').count
+puts "Stores with over 1 million in sales: #{@stores_with_over_million_sales}"
